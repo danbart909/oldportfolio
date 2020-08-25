cb0a8f41b0bb1faef5b0f8161d13297513211b87
Dan Bartlett <danbart909@gmail.com>
2020-08-25 18:22:36 -0400

    fixed bouncy height in projects

--- src/projects/Project3.js
+++ src/projects/Project3.js
@@ -4,44 +4,46 @@ export default class Project3 extends Component {
   render() {
     return (
       <div id='project3' className='project'>
-        <div id='project3-top' className='project-top'>
-          <div id='project3-title' className='project-title'>
-            <span>Thinkful Forum</span>
-          </div>
-          <div id='project3-links' className='project-links'>
-            <div id='project3-links-live' className='project-links-live'>
-              <a href="https://forum-client-delta.now.sh/" className='live-link' target="_blank">
-                <i className="icon live-i fas fa-globe" role="link" />
-              </a>
-              <a href="https://forum-client-delta.now.sh/" className='live-link' target="_blank">
-                <span className="live-span">Live</span>
-              </a>
+        <div className='tophalf'>
+          <div id='project3-top' className='project-top'>
+            <div id='project3-title' className='project-title'>
+              <span>Thinkful Forum</span>
             </div>
-            <div id='project3-links-client' className='project-links-client'>
-  						<a href="https://github.com/danbart909/forum-client" className='client-link' target="_blank">
-                <i className="icon github-i fab fa-github" role="link" />
-              </a>
-              <a href="https://github.com/danbart909/forum-client" className='client-link' target="_blank">
-                <span className="github-span-client">Client Repo</span>
-              </a>
-            </div>
-            <div id='project3-links-server' className='project-links-server'>
-              <a href="https://github.com/danbart909/forum-server" target="_blank" className="server-link">
-                <i className="icon github2-i fab fa-github-square" role="link" />
-              </a>
-              <a href="https://github.com/danbart909/forum-server" target="_blank" className="server-link">
-                <span className="github-span-server">Server Repo</span>
-              </a>
+            <div id='project3-links' className='project-links'>
+              <div id='project3-links-live' className='project-links-live'>
+                <a href="https://forum-client-delta.now.sh/" className='live-link' target="_blank">
+                  <i className="icon live-i fas fa-globe" role="link" />
+                </a>
+                <a href="https://forum-client-delta.now.sh/" className='live-link' target="_blank">
+                  <span className="live-span">Live</span>
+                </a>
+              </div>
+              <div id='project3-links-client' className='project-links-client'>
+                <a href="https://github.com/danbart909/forum-client" className='client-link' target="_blank">
+                  <i className="icon github-i fab fa-github" role="link" />
+                </a>
+                <a href="https://github.com/danbart909/forum-client" className='client-link' target="_blank">
+                  <span className="github-span-client">Client Repo</span>
+                </a>
+              </div>
+              <div id='project3-links-server' className='project-links-server'>
+                <a href="https://github.com/danbart909/forum-server" target="_blank" className="server-link">
+                  <i className="icon github2-i fab fa-github-square" role="link" />
+                </a>
+                <a href="https://github.com/danbart909/forum-server" target="_blank" className="server-link">
+                  <span className="github-span-server">Server Repo</span>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div id='project3-mid' className='project-mid'>
-          <div id='project3-description' className='project-description'>
-            <span>This is a fully reactive message board for a small team or group of people to communicate with each other. It's a little more permanent and less "quick" than a messaging app such as Slack, but is still a widely used method of online communication. I spent a lot of time on message boards in my youth, so creating a forum from scratch felt like an ideal project.</span>
-          </div>
-          <div id='project3-tech' className='project-tech'>
-            <span>Technologies:</span>
-            <span>HTML, CSS, Javascript, React, Express.js, Node.js, postgresql</span>
+          <div id='project3-mid' className='project-mid'>
+            <div id='project3-description' className='project-description'>
+              <span>This is a fully reactive message board for a small team or group of people to communicate with each other. It's a little more permanent and less "quick" than a messaging app such as Slack, but is still a widely used method of online communication. I spent a lot of time on message boards in my youth, so creating a forum from scratch felt like an ideal project.</span>
+            </div>
+            <div id='project3-tech' className='project-tech'>
+              <span>Technologies:</span>
+              <span>HTML, CSS, Javascript, React, Express.js, Node.js, postgresql</span>
+            </div>
           </div>
         </div>
         <div id='project3-bot' className='project-bot'>
--- src/projects/Project4.js
+++ src/projects/Project4.js
@@ -4,36 +4,38 @@ export default class Project4 extends Component {
   render() {
     return (
       <div id='project4' className='project'>
-        <div id='project4-top' className='project-top'>
-          <div id='project4-title' className='project-title'>
-            <span>US Census App</span>
-          </div>
-          <div id='project4-links' className='project-links'>
-            <div id='project4-links-live' className='project-links-live'>
-              <a href="https://danbart909.github.io/USCensusApp/" className='live-link' target="_blank">
-                <i className="icon live-i fas fa-globe" role="link" />
-              </a>
-              <a href="https://danbart909.github.io/USCensusApp/" className='live-link' target="_blank">
-                <span className="live-span">Live</span>
-              </a>
+        <div className='tophalf'>
+          <div id='project4-top' className='project-top'>
+            <div id='project4-title' className='project-title'>
+              <span>US Census App</span>
             </div>
-            <div id='project4-links-client' className='project-links-client'>
-						  <a href="https://github.com/danbart909/USCensusApp" className='client-link' target="_blank">
-                <i className="icon github-i fab fa-github" role="link" />
-              </a>
-              <a href="https://github.com/danbart909/USCensusApp" className='client-link' target="_blank">
-                <span className="github-span-client">Client Repo</span>
-              </a>
+            <div id='project4-links' className='project-links'>
+              <div id='project4-links-live' className='project-links-live'>
+                <a href="https://danbart909.github.io/USCensusApp/" className='live-link' target="_blank">
+                  <i className="icon live-i fas fa-globe" role="link" />
+                </a>
+                <a href="https://danbart909.github.io/USCensusApp/" className='live-link' target="_blank">
+                  <span className="live-span">Live</span>
+                </a>
+              </div>
+              <div id='project4-links-client' className='project-links-client'>
+                <a href="https://github.com/danbart909/USCensusApp" className='client-link' target="_blank">
+                  <i className="icon github-i fab fa-github" role="link" />
+                </a>
+                <a href="https://github.com/danbart909/USCensusApp" className='client-link' target="_blank">
+                  <span className="github-span-client">Client Repo</span>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div id='project4-mid' className='project-mid'>
-          <div id='project4-description' className='project-description'>
-            <span>This project uses the US Census Bureau's wealth of census information to construct an app that would return a wide variety of population information from all 50 states. The enormous collection of data would prove useful for anyone seeking population statistics for research (or how to interact with the census API), and it comes with a large pool of variables to choose from.</span>
-          </div>
-          <div id='project4-tech' className='project-tech'>
-            <span>Technologies:</span>
-            <span>HTML, CSS, Javascript, jQuery</span>
+          <div id='project4-mid' className='project-mid'>
+            <div id='project4-description' className='project-description'>
+              <span>This project uses the US Census Bureau's wealth of census information to construct an app that would return a wide variety of population information from all 50 states. The enormous collection of data would prove useful for anyone seeking population statistics for research (or how to interact with the census API), and it comes with a large pool of variables to choose from.</span>
+            </div>
+            <div id='project4-tech' className='project-tech'>
+              <span>Technologies:</span>
+              <span>HTML, CSS, Javascript, jQuery</span>
+            </div>
           </div>
         </div>
         <div id='project4-bot' className='project-bot'>
