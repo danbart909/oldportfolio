cfed1ed82a081c41def18087ffc35ec767a6ae1a
Dan Bartlett <danbart909@gmail.com>
2021-09-22 14:06:22 -0400

    bio update

--- src/Bio.js
+++ src/Bio.js
@@ -13,7 +13,7 @@ export default class Bio extends Component {
   				</div>
   				<div id='bio-2'>
   				  <div id="bio-content">
-              <span>My name is Dan Bartlett. I am a professional freelance web developer and a graduate of the Thinkful Full Stack Web Developer program. I have projects built using HTML, CSS, Javascript, jQuery, React, Node.js/Express.js, and postgresql. I'm interested in learning how to solve problems through code as efficiently as possible, either solo or while collaborating with a team. My willingness to put forth the effort necessary to overcome any challenge makes me a valuable addition to any programming team.</span>
+              <span>My name is Dan Bartlett. I am a full-stack web developer, mobile app developer, and a graduate of the Thinkful Full Stack Web Developer program. I have projects built using HTML, CSS, Javascript, jQuery, React, React Native, Node.js/Express.js/Knex.js, and postgresql. I'm interested in learning how to solve problems through code as efficiently as possible, either solo or while collaborating with a team. My willingness to put forth the effort necessary to overcome any challenge makes me a valuable addition to any programming team.</span>
     				</div>
             <div id='bio-footer'>
               <span>Download my Resume: <a href='/danielbartlettresume.docx'>.docx</a> or <a href='/danielbartlettresume.pdf'>.pdf</a></span>
